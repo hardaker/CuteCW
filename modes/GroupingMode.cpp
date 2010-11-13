2981d2ff1663e9f94589f4203976af6f5e9aa8f4
Wes Hardaker <opensource@hardakers.net>
2010-11-13 04:04:11 -0800

    unneeded code/copy cleanup of new mode

--- modes/GroupingMode.cpp
+++ modes/GroupingMode.cpp
@@ -9,9 +9,9 @@ void GroupingMode::switchToMode() {
     m_ui->letter->show();
     m_ui->play->show();
     m_ui->clearTraining->show();
-    m_ui->modeMenu->setText("Recognition Training");
+    m_ui->modeMenu->setText(tr("Grouping Training");
     m_ui->changeSequence->show();
-    m_ui->helpBar->setText("<font color=\"green\">Type the letter you hear ASAP.</font>");
+    m_ui->helpBar->setText(tr("<font color=\"green\">Type the groups of characters you hear ASAP.</font>"));
     clear();
 
     setupSequences();
@@ -21,10 +21,5 @@ void GroupingMode::switchToMode() {
 void GroupingMode::handleKeyPress(QChar letterPressed) {
     if (runningMode() != RUNNING)
         return;
-    // ensure we're not still playing a sound:
-    if (m_morse->audioMode() == Morse::PLAYING)
-        return;
-    // analyze they're keyed letter and immediately start playing a new one
-    TrainingMode::handleKeyPress(letterPressed);
-    startNextTrainingKey();
+
 }
