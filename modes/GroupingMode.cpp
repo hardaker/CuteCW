22242ae8c4d5ce19eea0271fcb0baa8dd3a20be1
Wes Hardaker <opensource@hardakers.net>
2010-12-04 08:54:43 -0800

    Made grouping mode work with increasing length

--- modes/GroupingMode.cpp
+++ modes/GroupingMode.cpp
@@ -3,7 +3,7 @@
 #include "modes/GroupingMode.h"
 
 GroupingMode::GroupingMode(Morse *parent, Ui::MainWindow *ui) :
-    TrainingMode(parent, ui), m_groupLength(DEFAULT_GROUPLENGTH)
+    TrainingMode(parent, ui)
 {
     createNextSequence();
 }
@@ -30,6 +30,9 @@ void GroupingMode::handleKeyPress(QChar letter) {
         return;
     }
 
+    if (m_currentSequence.length() == m_enteredWord.length()) // they already hit the length previously
+        return;
+
     // Copied from WordTraining (ish)
     if (m_currentSequence[m_enteredWord.length()] == letter) {
         m_ui->letter->setText(m_ui->letter->text() + "<font color=\"green\">" + letter + "<font>");
@@ -40,16 +43,27 @@ void GroupingMode::handleKeyPress(QChar letter) {
     m_enteredWord.append(letter);
     if (m_currentSequence.length() == m_enteredWord.length()) {
         if (m_wordWasGood) {
+            m_goodGuesses++;
             m_ui->letter->setText(m_ui->letter->text() + " - <font color=\"green\">GOOD</font>");
         } else {
+            m_badGuesses++;
             m_ui->letter->setText(m_ui->letter->text() + " - <font color=\"red\">FAIL (" + m_currentSequence + ")</font>");
         }
     }
+    setSequenceText();
+}
 
+void GroupingMode::setSequenceText() {
+    m_morse->m_sequenceLabel->setText(QString("current length = %1, good = %2, bad = %3").arg(m_groupLength).arg(m_goodGuesses).arg(m_badGuesses));
 }
 
 void GroupingMode::createNextSequence() {
     m_currentSequence = QString();
+
+    m_groupLength = DEFAULT_GROUPLENGTH + ((m_goodGuesses - m_badGuesses)/GROUPLENGTH_WEIGHT);
+    if (m_groupLength < 1)
+        m_groupLength = 1;
+
     float m_trainingLength = float(m_trainingSequence.length());
     for(int i = 0 ; i < m_groupLength; i++) {
         m_currentSequence += m_trainingSequence[int(float(m_trainingLength) * float(qrand())/float(RAND_MAX))];
@@ -61,9 +75,10 @@ void GroupingMode::startNextGroup()
     createNextSequence();
     m_morse->add(m_currentSequence);
     m_morse->maybePlaySequence();
-    m_ui->letter->setText("");
     m_wordWasGood = true;
     m_enteredWord = "";
+    setSequenceText();
+    m_ui->letter->setText(QString().number(m_groupLength) + ": ");
 }
 
 void GroupingMode::play()
@@ -78,3 +93,8 @@ bool GroupingMode::enterPressed()
     return true;
 }
 
+void GroupingMode::clear()
+{
+    m_goodGuesses = 0;
+    m_badGuesses = 0;
+}
--- modes/GroupingMode.h
+++ modes/GroupingMode.h
@@ -4,7 +4,8 @@
 #include "TrainingMode.h"
 #include "Morse.h"
 
-const int DEFAULT_GROUPLENGTH = 3;
+const int DEFAULT_GROUPLENGTH =1;
+const int GROUPLENGTH_WEIGHT = 4;
 
 class GroupingMode : public TrainingMode
 {
@@ -16,14 +17,18 @@ public:
     virtual void handleKeyPress(QChar letter);
 
     void createNextSequence();
+    void setSequenceText();
 
 public slots:
     void startNextGroup();
     virtual bool enterPressed();
     virtual void play();
+    virtual void clear();
 
 private:
     int     m_groupLength;
+    int     m_goodGuesses;
+    int     m_badGuesses;
     QString m_currentSequence;
     QString m_enteredWord;
     bool    m_wordWasGood;
