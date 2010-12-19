dfbc8f1a7df6dbd9ccb4495278b48edac4878676
Wes Hardaker <hardaker@users.sourceforge.net>
2010-12-18 22:23:28 -0600

    fixed class names

--- modes/games/WordGame.cpp
+++ modes/games/WordGame.cpp
@@ -2,7 +2,7 @@
 
 #include "modes/GroupGame.h"
 
-GroupGame::GroupGame(Morse *parent, Ui::MainWindow *ui) :
+WordGame::WordGame(Morse *parent, Ui::MainWindow *ui) :
   GroupingMode(parent, ui), MCountGameMode(), m_scores("Word Accuracy Game")
 {
     connect(this, SIGNAL(groupEntered(int, int)),
@@ -11,40 +11,39 @@ GroupGame::GroupGame(Morse *parent, Ui::MainWindow *ui) :
             this, SLOT(gameOver()));
 }
 
-void GroupGame::switchToMode() {
+void WordGame::switchToMode() {
     GroupingMode::switchToMode();
     m_ui->clearTraining->hide();
     m_ui->modeMenu->setText(tr("Word Game"));
     m_ui->helpBar->setText(tr("<font color=\"green\">Type the groups of characters you hear ASAP.</font>"));
 }
 
-void GroupGame::play()
+void WordGame::play()
 {
     startGame();
     enterPressed();
 }
 
-void GroupGame::gameOver()
+void WordGame::gameOver()
 {
   pause();
   m_scores.addScore(QString(), score());
 }
 
-void GroupGame::groupGuessed(int right, int total)
+void WordGame::groupGuessed(int right, int total)
 {
     nextTurn(right*10*((total==right)?2:1));
 }
 
-void GroupGame::setSequenceText()
+void WordGame::setSequenceText()
 {
   m_morse->m_sequenceLabel->setText(tr("current length = %1, good = %2, bad = %3, score = %4").arg(m_groupLength).arg(m_goodGuesses).arg(m_badGuesses)).arg(score());
 }
 
-QString GroupMode::helpText()
+QString WordGame::helpText()
 {
     return tr("Characters in the chosen training sequence will be keyed starting with a single character.  "
               "Type in the characters you hear in the group.  Your results good or bad will be displayed "
               "on the screen in green/red colors indicating good/bad guesses."
               "<p>As you guess more and more groups correctly the number of characters in the group will slowly go upward.");
 }
-
