fb2eddd058b772e80c36faacb7e8117e910dfa94
Wes Hardaker <opensource@hardakers.net>
2011-04-06 07:41:33 -0700

    draw the yellow boxes to show the keyed and timing

--- widgets/KeyTimingDisplay.cpp
+++ widgets/KeyTimingDisplay.cpp
@@ -1,6 +1,7 @@
 #include "KeyTimingDisplay.h"
 
 #include <QtGui/QPainter>
+#include <qdebug.h>
 
 KeyTimingDisplay::KeyTimingDisplay(QWidget *parent) :
     QWidget(parent)
@@ -11,11 +12,42 @@ void KeyTimingDisplay::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
     painter.save();
-    if (m_keyedTimings.count() > 0)
-        painter.setPen(Qt::red);
-    else
-        painter.setPen(Qt::blue);
-    painter.drawRect(5,5,width()-10,height()-10);
+
+    int minx = 10, miny = 0, maxx = width() - 10, maxy = height();
+    int widgetWidth = maxx - minx, widgetHeight = height();
+    int lineHeight = 10;
+
+    painter.setBrush(Qt::black);
+    painter.drawRect(0, 0, width(), height());
+
+    if (m_keyedTimings.length() > 0) {
+        int biggestTime = qMax(m_keyedTimings.last(), m_requiredTimings.last());
+
+        painter.setBrush(Qt::yellow);
+        QList<int>::const_iterator spot = m_requiredTimings.begin();
+        QList<int>::const_iterator end = m_requiredTimings.end();
+
+        QList<int>::const_iterator keyedSpot = m_keyedTimings.begin();
+        QList<int>::const_iterator keyedEnd = m_keyedTimings.end();
+
+        while(spot != end) {
+            QList<int>::const_iterator startingSpot = spot;
+            QList<int>::const_iterator keyedStartingSpot = keyedSpot;
+
+            // draw the required timing box that is the perfect keying
+            spot++;
+            painter.drawRect(minx + (widgetWidth * *startingSpot)/biggestTime,    lineHeight,
+                             (widgetWidth * (*spot - *startingSpot))/biggestTime, lineHeight);
+            spot++;
+
+            // draw the box that was actually keyed
+            keyedSpot++;
+            painter.drawRect(minx + (widgetWidth * *keyedStartingSpot)/biggestTime,          3*lineHeight,
+                             (widgetWidth * (*keyedSpot - *keyedStartingSpot))/biggestTime,  lineHeight);
+            keyedSpot++;
+        }
+    }
+
     painter.restore();
 }
 
