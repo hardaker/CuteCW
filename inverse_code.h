305722b0f87cb6dd105816c8b5e98eef199e02d5
Wes Hardaker <opensource@hardakers.net>
2011-05-21 20:50:02 -0700

    Failed attempt at storing QList's inside of a QHash.

---
+++ inverse_code.h
@@ -0,0 +1,163 @@
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = 'a';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = 'b';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DAH << Morse::DIT;
+inverseCode[qlist] = 'c';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = 'd';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT;
+inverseCode[qlist] = 'e';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DAH << Morse::DIT;
+inverseCode[qlist] = 'f';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DIT;
+inverseCode[qlist] = 'g';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = 'h';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = 'i';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DAH << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = 'j';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = 'k';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DAH << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = 'l';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = 'm';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT;
+inverseCode[qlist] = 'n';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = 'o';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DAH << Morse::DAH << Morse::DIT;
+inverseCode[qlist] = 'p';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = 'q';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DAH << Morse::DIT;
+inverseCode[qlist] = 'r';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = 's';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH;
+inverseCode[qlist] = 't';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = 'u';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = 'v';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = 'w';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = 'x';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = 'y';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = 'z';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DAH << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = '0';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DAH << Morse::DAH << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = '1';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DAH << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = '2';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = '3';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = '4';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = '5';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = '6';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DIT << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = '7';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DAH << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = '8';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DAH << Morse::DAH << Morse::DIT;
+inverseCode[qlist] = '9';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DAH << Morse::DIT << Morse::DAH << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = '.';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DAH << Morse::DIT << Morse::DIT << Morse::DAH << Morse::DAH;
+inverseCode[qlist] = ',';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DIT << Morse::DAH << Morse::DIT;
+inverseCode[qlist] = '/';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DIT << Morse::DIT << Morse::DAH << Morse::DAH << Morse::DIT << Morse::DIT;
+inverseCode[qlist] = '?';
+
+qlist = QList<Morse::ditdah>();
+qlist << Morse::DAH << Morse::DIT << Morse::DIT << Morse::DIT << Morse::DAH;
+inverseCode[qlist] = '=';
