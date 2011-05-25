138c3e07b7415a3b7037b109ae020d19031479a7
Wes Hardaker <opensource@hardakers.net>
2011-05-24 22:29:36 -0700

    beginning of drawwing a letter

--- modes/games/Invader.cpp
+++ modes/games/Invader.cpp
@@ -9,26 +9,27 @@ Invader::Invader(QGraphicsWidget *parent, const QString &text) :
 
 QRectF Invader::boundingRect() const
 {
-    return QRectF(-5, -5, 20, 20);
+    return QRectF(0, 0, 20, 20);
 }
 
 QPainterPath Invader::shape() const
 {
     QPainterPath path;
-    path.addEllipse(-5, -5, 20, 20);
+    path.addEllipse(0, 0, 20, 20);
     return path;
 }
 
 void Invader::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *)
 {
     painter->setPen(Qt::NoPen);
-    painter->setBrush(Qt::darkGray);
+    painter->setBrush(Qt::lightGray);
     painter->drawEllipse(-5, -5, 20, 20);
 
     QFont font = painter->font();
-    font.setPointSize(4);
+    font.setPointSize(16);
     painter->setFont(font);
-    painter->drawText(QRectF(-5, -5, 20, 20), m_text);
+    painter->setPen(Qt::red);
+    painter->drawText(QRectF(0,0,20,20), Qt::AlignHCenter | Qt::AlignVCenter, m_text);
 }
 
 QVariant Invader::itemChange(GraphicsItemChange change, const QVariant &value)
